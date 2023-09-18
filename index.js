228cf822695f3135f032046eec133461ba92ea29
Acan <xie.zeyu20@gmail.com>
2023-09-18 14:34:04 +0800

    .

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ fetch("./config.json").then(config => config.json()).then(config => {
         .then(data => data.json())
         .then(data => {
             data.forEach(item => {
-                const _repo_item = document.createElement("li")
-                _repo_item.innerHTML = `<a href="${item.owner.html_url + "/" + item.name}">${item.name}</a>`
+                const _repo_item = document.createElement("tr")
+                _repo_item.innerHTML = `
+                    <td><a href="${item.html_url}">${item.name}</a></td>
+                    <td><a href="${item.homepage}">Link</a></td>
+                    <td onclick="navigator.clipboard.writeText('${item.ssh_url}')">Copy</td>
+                `
                 _repo_lists.appendChild(_repo_item)
-            });
+            })
         }).catch(err_ => {
             console.log("ERROR" + err_)
         })
