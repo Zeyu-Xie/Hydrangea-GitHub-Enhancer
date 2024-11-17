b84ad5a1e392de6c673c693fb014ee649e56741e
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-11-17 21:06:06 +0100

    Fixed the python developing tools

--- python developer tools/icon/icon.py
+++ python developer tools/icon/icon.py
@@ -1,10 +1,17 @@
 import json
 import os
+import xml.etree.ElementTree as ET
 
 data = []
 with open(os.path.join(os.path.dirname(__file__), "icon.json")) as f:
     data = json.load(f)
 
+def resize_svg(input_string, width="16px", height="16px"):
+    tree = ET.ElementTree(ET.fromstring(input_string))
+    root = tree.getroot()
+    root.set("width", width)
+    root.set("height", height)
+    return ET.tostring(root, encoding="unicode")
 
 def process_icon(icon_data):
     output = {}
@@ -16,6 +23,7 @@ def process_icon(icon_data):
         svg_code = ""
         with open(path) as f:
             svg_code = f.read()
+        svg_code = resize_svg(svg_code)
         for ext in ext_list:
             output[ext] = svg_code
     with open(os.path.join(os.path.dirname(__file__), "output.json"), "w") as f:
